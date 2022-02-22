ddb2933bbe9e4115cd5bbf507b827c4c1b48ba98
amogh54 <pawaskaramogh54@gmail.com>
2022-02-22 09:57:50 +0530

    v2

---
+++ soft/getaudio.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget  http://vmhost/www/scripts/logo/pulse.desktop -P /usr/local/share/applications/
+
---
+++ soft/getlagging.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget  http://vmhost/www/scripts/logo/lagging.desktop -P /usr/local/share/applications/
+
